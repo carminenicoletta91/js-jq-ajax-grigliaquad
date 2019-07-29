b00c3c50bf1b89ca2f79bcb3660383f2595f4ff4
carminenicoletta91 <carminenicolettakr91@gmail.com>
2019-07-29 16:09:43 +0200

    --inserita funzione al click con annessa chiamata ajax alla mia api--

--- main.js
+++ main.js
@@ -2,3 +2,38 @@
 // che prende un numero random da 1 a 9.
 // Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
 // Il numero ottenuto appare al centro del quadrato
+$(document).ready(function(){
+
+  // creo un evento di tipo click per tutti i quadrati della mia griglia
+  $(".box-griglia .riga .box-quadrato").click(
+    function(){
+      // salvo l'oggetto cliccato in una variabile
+      var mioquadrato = $(this);
+      // chiamata ajax alla mia api
+      $.ajax({
+        url: "https://flynn.boolean.careers/exercises/api/random/int",
+        method: "GET",
+        success: function(data){//se l'esito della chiamata è positivo
+          console.log(data.response);
+
+          if(data.response <= 5){// prima condizione se il numero che mi ritorna e <=5
+            console.log("sei nel giallo");
+            // aggiungo la classe per lo sfondo giallo
+            mioquadrato.addClass('yellow-box');
+            // inserisco il numero nel mio quadrato
+            mioquadrato.text(data.response);
+          }else{//se il numero va da 5 a 9
+            console.log("sei nel verde");
+            // aggiungo la classe per lo sfondo verde
+            mioquadrato.addClass('green-box');
+            //inserisco il numero nel mio quadrato
+            mioquadrato.text(data.response);
+          }
+        }//chiusura funzione in caso di successo
+
+
+
+      });//chiusura chiamata api
+    })//chiusura funzione al click
+
+})//chiusura funzione document ready
